70f7571e374ae489255222f21998dda89fe01bb7
Asabeaaa <anniea.boadu2013@gmail.com>
2023-11-15 11:43:58 +0000

    user number or transactions update flow added

--- database/helper.py
+++ database/helper.py
@@ -5,6 +5,7 @@ from typing import Dict, List
 from sqlalchemy import select
 import pandas as pd
 import uuid
+from sqlalchemy import update
 
 
 class UpdateDB(Connector):
@@ -22,8 +23,21 @@ class UpdateDB(Connector):
         finally:
             session.close()
 
+    def update_user_transaction(self, number_of_transactions: int, user_uuid: uuid.uuid4):
+        try:
+            factory = sessionmaker(bind=self.pg_engine)
+            session = factory()
+            session.query(User).filter(User.uuid == user_uuid).update(
+                {"nTransactions": number_of_transactions})
+            session.commit()
+
+        except Exception as e:
+            print(f"Error: {e}")
+
+        finally:
+            session.close()
+
     def insert_user(self, row: pd.Series):
-        # print(row)
         try:
             factory = sessionmaker(bind=self.pg_engine)
             session = factory()
@@ -39,18 +53,27 @@ class UpdateDB(Connector):
             # insert transaction
             self.insert_transaction(row, new_user_uuid)
 
+            # update user transaction to 1
+            self.update_user_transaction(1, new_user_uuid)
+
         except Exception as e:
             if "duplicate key value violates unique constraint" in str(e):
                 print("User already exists")
 
                 # retrieve user with that phone number
-                user = self.retrieve_from_db(stmt=select(User.uuid).where(
+                user = self.retrieve_from_db(stmt=select(User.uuid, User.nTransactions).where(
                     User.phoneNumber == row["agentPhoneNumber"]))
 
                 old_user_uuid = user[0]["uuid"]
+                number_of_user_trnxs = user[0]["nTransactions"]
 
                 # insert transaction
                 self.insert_transaction(row, old_user_uuid)
+
+                # update user transaction + 1
+                self.update_user_transaction(
+                    number_of_user_trnxs + 1, old_user_uuid)
+
             else:
                 print(f"Error:{e}")
 
